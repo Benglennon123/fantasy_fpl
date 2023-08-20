b3506fcf5d95c5f9f5f471d37b236e0709be5354
Benglennon123 <glennonben@gmail.com>
2023-08-20 19:46:42 +0100

    adding variable function

--- fpl_data/player_data.py
+++ fpl_data/player_data.py
@@ -239,11 +239,76 @@ def pick_team(df,inlcude_player_ids)-> pd.DataFrame():
     selected = [LpVariable(f"player_{i}", cat='Binary') for i in range(len(df))]
 
     # Define the objective function (maximize total points)
-    prob += lpSum(selected[i] * df.loc[i, 'average_points'] for i in df.index) + \
-            lpSum(selected[i] * df.loc[i, 'total_points_last_season'] for i in range(len(df))) + \
-            lpSum(selected[i] * df.loc[i, 'form'] for i in range(len(df)))
+    prob += lpSum(selected[i] * df.loc[i, 'total_points_last_season'] for i in range(len(df))) + \
+            lpSum(selected[i] * df.loc[i, 'form'] for i in range(len(df))) + \
+            lpSum(selected[i] * df.loc[i, 'average_win_perc_first_10'] for i in range(len(df))) 
     
     #lpSum(selected[i] * df.loc[i, 'total_points_last_season'] for i in range(len(df))) + \
+    
+
+    # Add budget constraint
+    prob += lpSum(selected[i] * df.loc[i, 'now_cost'] for i in range(len(df))) <= budget
+
+    # Add position constraints
+    for position in positions:
+        prob += lpSum(selected[i] for i in range(len(df)) if df.loc[i, 'position'] == position) == position_counts[position]
+
+    # Add constraint for maximum players from the same team
+    team_players = {team: lpSum(selected[i] for i in df.index if df.loc[i, 'team'] == team)
+                    for team in df['team'].unique()}
+    for team, players in team_players.items():
+        prob += players <= max_players_per_team
+
+    # Add specific player selections
+    if len(inlcude_player_ids)> 0:
+        # Add specific player selections based on IDs
+        for i in df.index:
+            if df.loc[i, 'id'] in inlcude_player_ids:
+                prob += selected[i] == 1
+
+    # Solve the problem
+    prob.solve()
+
+    # Get the selected players
+    selected_players = [df.loc[i] for i in range(len(df)) if selected[i].varValue == 1]
+
+
+    return pd.DataFrame(selected_players)
+
+def pick_team_variable(df,inlcude_player_ids,variables_map)-> pd.DataFrame():
+    """" Selects a team based on multiple different criteria 
+    
+    :param df: a pandas dataframe of all the static player informtion
+    :param inlcude_player_ids: a list of player ids that must be in the team 
+    :param variables_list: a dictionary of variables and whether we should max or minimize for them 
+    returns a dataframe of 15 players for your team
+    """
+    
+    df = df[df.chance_of_playing_next_round == 100]
+    #removing Raya as he is transfering
+    df = df[df.id != 113]
+    df = df[df.team != 12]
+    df = df.reset_index(drop=True)
+
+    # Set budget constraint and position constraints
+    budget = 1000  # Set your budget limit
+    positions = ['Goalkeeper', 'Defender', 'Midfielder', 'Attacker']
+    position_counts = {'Goalkeeper': 2, 'Defender': 5, 'Midfielder': 5, 'Attacker': 3}
+    max_players_per_team = 3
+
+    # Create a PuLP linear programming problem
+    prob = LpProblem("FantasyFootball", LpMaximize)
+
+    # Create LpVariables for player selection
+    selected = [LpVariable(f"player_{i}", cat='Binary') for i in range(len(df))]
+
+    # Define the objective function (maximize total points)
+    for var in variables_map:
+        if variables_map[var] =="min":
+            df[var] = df[var].astype(float).fillna(0)
+            prob = prob - lpSum(selected[i] * df.loc[i, variables_map] for i in range(len(df)))
+        else:
+            prob = prob + lpSum(selected[i] * df.loc[i, variables_map] for i in range(len(df)))
     
 
     # Add budget constraint
@@ -293,3 +358,12 @@ def check_teams_performance(player_ids,gameweek_num:int)-> pd.DataFrame():
         
         points_map[player] = points
     return points_map
+
+def get_best_team(gameweek_num:int):
+    """ Retreives the best performing team of that game week 
+
+    :param gameweek_num : The gameweek you want to check the best perfoming team for 
+    returns the best possible team
+    """
+    df = pd.DataFrame(r.get("https://fantasy.premierleague.com/api/dream-team/"+str(gameweek_num)).json()['team'])
+    return df 
