09efe1a99075559834dc0812a5b972bd8f9d0a9c
Benglennon123 <glennonben@gmail.com>
2023-09-16 22:24:04 +0100

    Adding optimizer logic

--- fpl_data/evals.py
+++ fpl_data/evals.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from .player_data import get_player_data
+from .models import variable_team
 import requests as r
 
 
@@ -54,3 +55,59 @@ def compare_performance(
     )
     print("Models Team Performance: " + str(df["points_adjusted"].sum()))
     print("Best Team Performance: " + str(get_best_team(gameweek_num)["points"].sum()))
+
+
+def get_performance(
+    df: pd.DataFrame,
+    gameweek_num: int = 1,
+    captain_map: dict = {},
+    bench_map: dict = {},
+):
+    df["gameweek_points"] = df["id"].map(
+        check_teams_performance(list(df["id"]), gameweek_num)
+    )
+    df["captain"] = df["id"].map(captain_map).fillna(1).astype(int)
+    df["points_adjusted"] = df["gameweek_points"] * df["captain"]
+    df["bench"] = df["id"].map(bench_map)
+    df["points_adjusted"] = df.apply(
+        lambda x: 0 if x["bench"] == 1 else x["points_adjusted"], axis=1
+    )
+    return df["points_adjusted"].sum()
+
+
+def calc_avg_weekly_score(
+    df: pd.DataFrame,
+    captain_map: dict = {},
+    bench_map: dict = {},
+    gameweek_start: int = 1,
+    gameweek_end: int = 4,
+) -> int:
+    """A function to get the average weekly performance of a team selected by the model"""
+    points_list = []
+    for i in range(gameweek_start, gameweek_end):
+        points_list.append(get_performance(df, i, captain_map, bench_map))
+
+    return sum(points_list) / len(points_list)
+
+
+def reward_func(
+    coeffients: list,
+    df: pd.DataFrame,
+    include_players: list,
+    var_dict: dict,
+    captain_map: dict = {},
+    bench_map: dict = {},
+    gameweek_start: int = 1,
+    gameweek_end: int = 4,
+):
+    if len(coeffients) == len(var_dict):
+        for key, value in zip(var_dict.keys(), coeffients):
+            var_dict[key] = value
+    else:
+        print("Number of values does not match the number of keys.")
+
+    team_selected = variable_team(df, var_dict, include_players)
+    avg_score = calc_avg_weekly_score(
+        team_selected, captain_map, bench_map, gameweek_start, gameweek_end
+    )
+    return -avg_score
